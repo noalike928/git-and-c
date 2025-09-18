8d782ce31a74370b7d59ccd75a1552beb313f35f
noalike <amer67682@gmail.com>
2025-09-18 11:28:30 +0300

    added the main file

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stdexcept>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+
+auto clearScreen = []()
+{
+    #ifdef _WIN32
+  system("cls");
+    #else
+  system("clear");
+    #endif
+
+  // didn,t use the ANSII clear screen way because don,t remember it=_=
+};
+
+auto wait = [](const int& sec)
+{
+
+    std::this_thread::sleep_for(std::chrono::seconds(sec));
+};
+
+static const int getUserInput()
+{
+    int userNum = 0;
+
+    while (true)
+    {
+      clearScreen();
+
+      std::cout << "Please Enter The Number Of Iterations:";
+
+      if (!(std::cin >> userNum))
+      {
+        std::cin.ignore();
+        std::cin.clear();
+
+        std::cerr << "Can,t Handle None Numerical Types";
+
+        wait(3);
+
+        continue;
+      }
+
+      break;
+    }
+
+    return userNum;
+}
+
+int main()
+{
+    int userNum = getUserInput();
+
+    for (int i = 0; i <= userNum; ++i)
+    {
+        std::cout << i << '\n';
+    }
+
+    return 0;
+}
